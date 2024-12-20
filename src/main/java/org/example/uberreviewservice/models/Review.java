0980af644325ae75b764d5caf28c5be93f0ea098
Rohith Janwadkar <rohithjanwadkar@Rohiths-MacBook-Air.local>
2024-12-20 00:00:20 -0600

    Added more columns for review table

--- src/main/java/org/example/uberreviewservice/models/Review.java
+++ src/main/java/org/example/uberreviewservice/models/Review.java
@@ -1,10 +1,31 @@
 package org.example.uberreviewservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name="bookingreview ")
 public class Review {
     @Id //This annotation makes this variable primary key of our table
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate//handle it for object creation
+    Date createdAt;
+
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate//for object updates
+    Date updatedAt;
 }
