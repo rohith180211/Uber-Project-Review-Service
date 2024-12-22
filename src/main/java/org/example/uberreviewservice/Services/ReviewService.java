3a178484d82078f31966ebc69b41e8fece68ca51
Rohith Janwadkar <rohithjanwadkar@Rohiths-MacBook-Air.local>
2024-12-22 13:35:14 -0600

    Added booking associated review

--- src/main/java/org/example/uberreviewservice/Services/ReviewService.java
+++ src/main/java/org/example/uberreviewservice/Services/ReviewService.java
@@ -1,30 +1,44 @@
 package org.example.uberreviewservice.Services;
 
+import org.example.uberreviewservice.Repositories.BookingRepository;
 import org.example.uberreviewservice.Repositories.ReviewRepository;
+import org.example.uberreviewservice.models.Booking;
 import org.example.uberreviewservice.models.Review;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 
 public class ReviewService implements CommandLineRunner {
     private final ReviewRepository reviewRepository;
+    private final BookingRepository bookingRepository;
 
-    public ReviewService(ReviewRepository reviewRepository) {
+    public ReviewService(ReviewRepository reviewRepository, BookingRepository bookingRepository) {
         this.reviewRepository = reviewRepository;
+        this.bookingRepository = bookingRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Review r=Review.builder().content("Amazing").rating(4.0).build();//code to create plain java obj
-        System.out.println(r);
-        reviewRepository.save(r);//This code executes sql query
-        List<Review> reviews=reviewRepository.findAll();
-        for (Review review : reviews) {
-            System.out.println(review.getContent());
+//        Review r=Review.builder().content("Amazing").rating(4.0).build();
+//        Booking b=Booking.builder().review(r).endTime(new Date()).build();
+////code to create plain java obj
+//
+//        bookingRepository.save(b);
+//        reviewRepository.save(r);//This code executes sql query
+//
+//        System.out.println(r);
+//        List<Review> reviews=reviewRepository.findAll();
+//        for (Review review : reviews) {
+//            System.out.println(review.getContent());
+//        }
+        Optional<Booking> b=bookingRepository.findById(6L);
+        if(b.isPresent()){
+            bookingRepository.deleteById(b.get().getId()) ;
         }
     }
 }
---
+++ src/main/java/org/example/uberreviewservice/models/Booking.java
@@ -0,0 +1,29 @@
+package org.example.uberreviewservice.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking extends BaseModel{
+
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
+    private Review review;//Defined a one -to -one relationship
+
+    @Enumerated(value=EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date starTime;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date endTime;
+
+    private long totalDistance;
+}
